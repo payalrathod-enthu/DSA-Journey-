bbece3f7d87f6014948a964e7683f2a9f8a1584b
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2026-09-09 09:32:35 +0530

    Leetcode #118

---
+++ Pascal's_Triangle.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        triangle = []
+
+        for row_num in range(numRows):
+            row = [1] * (row_num + 1)  # every row starts and ends with 1
+
+            # fill in the middle values (skip first and last, already 1)
+            for j in range(1, row_num):
+                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+
+            triangle.append(row)
+
+        return triangle
